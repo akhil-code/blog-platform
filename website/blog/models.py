63f0e23c9f111c30abcc392fce90bedc04945c17
Akhil Guttula <guttula.akhil@gmail.com>
2018-08-11 23:07:56 +0530

    successfully implemented models required

--- website/blog/models.py
+++ website/blog/models.py
@@ -10,14 +10,6 @@ class Author(models.Model):
     def __str__(self):
         return f"{self.name}"
 
-class Blog(models.Model):
-    title = models.CharField(max_length=255, blank=False)
-    body = models.TextField(blank=False)
-    author = models.ForeignKey('Author', on_delete=models.CASCADE, blank=False)
-    date_posted = models.DateTimeField(auto_now_add=True, blank=False)
-
-    def __str__(self):
-        return f"{self.title} by {self.author}"
 
 class Category(models.Model):
     name = models.CharField(max_length=255, blank=False)
@@ -26,13 +18,25 @@ class Category(models.Model):
     def __str__(self):
         return f"{self.name}"
 
+
 class Tag(models.Model):
     name = models.CharField(max_length=255, blank=True)
     views = models.IntegerField(default=0)
     category = models.ForeignKey('Category', on_delete=models.CASCADE, blank=False)
-
+    
     def __str__(self):
         return f"{self.name}"
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255, blank=False)
+    body = models.TextField(blank=False)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE, blank=False)
+    date_posted = models.DateTimeField(auto_now_add=True, blank=False)
+    tags = models.ManyToManyField(Tag, blank=False, related_name='blogs')
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
 
 class Comment(models.Model):
     author = models.ForeignKey('Author', on_delete=models.CASCADE, blank=False)
